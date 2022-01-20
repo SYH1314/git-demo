67a7ebb22b359c900331b5fa79936e402661e393
SYH1314 <735415630@qq.com>
2022-01-20 17:28:11 +0800

    third commit

--- src/main/java/com/learn/git/gitdemo/GitDemoApplication.java
+++ src/main/java/com/learn/git/gitdemo/GitDemoApplication.java
@@ -8,6 +8,7 @@ public class GitDemoApplication {
 
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("hello2");
         SpringApplication.run(GitDemoApplication.class, args);
     }
 
